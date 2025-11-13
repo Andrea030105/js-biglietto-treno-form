a70b4113392a2b1f3657dfe6da69e8028608e497
Andrea030105 <tummoloandrea05@gmail.com>
2025-11-13 16:03:07 +0100

    ex-done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,79 @@
-let eta = parseInt(prompt("Inserisci la tua età"));
-let km = parseInt(prompt("Inserisci i chilometri da percorrere"));
+const submit = document.getElementById("submit");
+const reset = document.getElementById("reset");
+
+reset.addEventListener("click", function () {
+
+    document.getElementById("nome").value = "";
+
+    document.getElementById("km").value = "";
+
+    document.getElementById("stampa-nome").innerHTML = "###### ######";
+
+    document.getElementById("stampa-offerta").innerHTML = "######";
+
+    document.getElementById("stampa-carrozza").innerHTML = "######";
+
+    document.getElementById("stampa-codice").innerHTML = "######";
+
+    document.getElementById("stampa-prezzo").innerHTML = "######";
+
+})
+
+submit.addEventListener("click", function () {
+    let nome = document.getElementById("nome").value;
+    let km = document.getElementById("km").value;
+    let eta = document.getElementById("eta").value;
+    let prezzo_calcolato
+    let offerta
+
+    console.log(nome, km, eta);
+
+    if (eta == 1) {
+
+        let prezzo = (0.21 * km * 0.8).toFixed(2);
+        prezzo_calcolato = prezzo
+        offerta = "20%"
+        console.log("sei minorenne, 20% di sconto " + prezzo);
+
+    } else if (eta == 3) {
+
+        let prezzo = (0.21 * km * 0.6).toFixed(2);
+        prezzo_calcolato = prezzo
+        offerta = "40%"
+        console.log("sei over 65, 40% di sconto " + prezzo);
+
+    } else {
+
+        let prezzo = (0.21 * km).toFixed(2);
+        prezzo_calcolato = prezzo
+        console.log("prezzo pieno " + prezzo);
+
+    }
+
+    document.getElementById("stampa-nome").innerHTML = nome;
+    document.getElementById("stampa-offerta").innerHTML = offerta;
+
+    document.getElementById("stampa-carrozza").innerHTML = Math.floor(Math.random() * 10);
+
+    document.getElementById("stampa-codice").innerHTML = Math.floor(Math.random() * 90000) + 10000;
+
+    document.getElementById("stampa-prezzo").innerHTML = prezzo_calcolato;
+
+    if (nome == "" & km == "") {
+        alert("devi inserire prima i campi");
+
+        document.getElementById("stampa-nome").innerHTML = "###### ######";
+
+        document.getElementById("stampa-offerta").innerHTML = "######";
+
+        document.getElementById("stampa-carrozza").innerHTML = "######";
+
+        document.getElementById("stampa-codice").innerHTML = "######";
+
+        document.getElementById("stampa-prezzo").innerHTML = "######";
 
 
-if (eta <= 18) {
+    }
 
-    let prezzo = (0.21 * km * 0.8).toFixed(2)
-    document.getElementById("prezzo").innerHTML = `${"sei minorenne, 20% di sconto"} ${prezzo}`;
-    console.log("sei minorenne, 20% di sconto " + prezzo);
-
-} else if (eta > 65) {
-
-    let prezzo = (0.21 * km * 0.6).toFixed(2)
-    document.getElementById("prezzo").innerHTML = `${"sei over 65, 40% di sconto"} ${prezzo}`;
-    console.log("sei over 65, 40% di sconto " + prezzo);
-
-} else {
-    let prezzo = (0.21 * km * 0.8).toFixed(2)
-    document.getElementById("prezzo").innerHTML = `${"Il tuo prezzo è"} ${prezzo}`;
-
-    console.log("prezzo pieno " + prezzo);
-
-}
+})
 
